044d146891c1b97695934cdfba2f8cf4fb01f766
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-27 23:35:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let text ="";
+  function textAdd(){
+    text = document.getElementById('a').value;
+  }
   return (
     <div>
         <p>Enter your name: </p>
-        <input onInput={text = this.value}></input>
+        <input onInput={textAdd} id="a"></input>
         <p id="p">Hello {text}!</p>
     </div>
   )
