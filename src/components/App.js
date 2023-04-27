4c33d476429407c5d1009b24e7a3017aa31b8300
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-27 23:31:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name: </p>
+        <input onInput={text = this.value}></input>
+        <p id="p">Hello {text}!</p>
     </div>
   )
 }
