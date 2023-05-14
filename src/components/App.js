3dca1b7b297d4a9df9370c9c3bd42bef4baaeed2
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-05-14 13:14:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-
+import { useState } from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let text ="";
+  let [text, setText] = useState('');
+  let textTemp = "";
   function textAdd(){
-    text = document.getElementById('a').value;
+    textTemp = document.getElementById('a').value;
+    setText(textTemp);
   }
   return (
     <div>
         <p>Enter your name: </p>
         <input onInput={textAdd} id="a"></input>
-        <p id="p">Hello {this.text}!</p>
+        <p id="p">Hello {textTemp}!</p>
     </div>
   )
 }
